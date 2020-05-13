26f17c662ec304c204151083c9714217fc4dd36b
zhouzhipeng <254790730@qq.com>
2020-05-13 17:47:44 +0800

    项目启动输出swagger地址

--- demoProject1/web/src/main/java/com/zzp/Application.java
+++ demoProject1/web/src/main/java/com/zzp/Application.java
@@ -1,10 +1,16 @@
 package com.zzp;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 /**
  * message
@@ -15,9 +21,17 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableSwagger2
+@Slf4j
 public class Application {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+    public static void main(String[] args) throws UnknownHostException {
+
+        ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
+        Environment env = context.getEnvironment();
+        String ip = InetAddress.getLocalHost().getHostAddress();
+        String port = env.getProperty("server.port");
+        String path = env.getProperty("server.servlet.context-path");
+        path = "/".equals(path) ? "" : path;
+        log.info("Swagger UI: \thttp://" + ip + ":" + port + "/swagger-ui.html");
     }
 }
